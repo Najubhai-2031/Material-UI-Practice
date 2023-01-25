b9a5d42cc7d4f55682d88e849fa3b6b91c9c6664
narin-stack <narinb.stacksum@gmail.com>
2023-01-25 15:37:01 +0530

    Update : Lauout Added

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
+import Header from "../Components/Header";
 import AuthanticationLayout from "../Layout/AuthanticationLayout";
+import MainLayout from "../Layout/MainLayout";
 import { SignIn } from "../pages/signIn";
 import { SignUp } from "../pages/signUp";
 
@@ -9,6 +11,13 @@ const Router = [
     children: [
       { path: "sign-in", element: <SignIn /> },
       { path: "sign-up", element: <SignUp /> },
+    ],
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Header /> },
     ],
   },
 ]
